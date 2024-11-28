5faf038e6d6e8fa022583a082338613b20443d85
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-11-28 19:59:27 +0100

    Added updated and delete variables

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const users = [];
@@ -15,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.post("/register", async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, role } = req.body;
 
     const existingUser = users.find((user) => user.email === email);
 
@@ -28,10 +30,64 @@ app.post("/register", async (req, res) => {
     users.push({
       email,
       password: hashedPassword,
+      role: role || "user",
     });
 
     console.log(users);
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/change-password", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = users.find((user) => user.email === email);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+    user.mustChangePassword = false;
+
+    res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/delete-account", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userIndex = users.findIndex((user) => user.email === email);
+
+    if (userIndex === -1) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      users[userIndex].password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    users.splice(userIndex, 1);
+
+    res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
