35ed9389ab5bdb3ffdb2e3a42d138fe1f16d134f
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-11-28 20:10:42 +0100

    Final commit

--- server.js
+++ server.js
@@ -41,6 +41,24 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.get("/admin", (req, res) => {
+  const { email } = req.query;
+
+  const user = users.find((user) => user.email === email);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  if (user.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "Access denied, admin role required" });
+  }
+
+  res.status(200).json({ message: "Welcome to the admin area!" });
+});
+
 app.post("/change-password", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -88,6 +106,35 @@ app.post("/delete-account", async (req, res) => {
     users.splice(userIndex, 1);
 
     res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/change-email", async (req, res) => {
+  try {
+    const { email, password, newEmail } = req.body;
+
+    const user = users.find((user) => user.email === email);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const existingEmail = users.find((user) => user.email === newEmail);
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    user.email = newEmail;
+
+    res.status(200).json({ message: "Email updated successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
